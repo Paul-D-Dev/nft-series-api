f370c7e28b2ddc6984bc5a52c5c01fd8a9b2f468
Paul DAO <dao.van.paul@gmail.com>
2023-11-24 13:50:32 -0500

    FEAT mysql connection

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,11 @@
 import { Application } from "express";
 import expressLoader   from './express';
+import dbLoader        from './mysql';
 
 export default async (app: Application) => {
   await expressLoader(app);
   console.log('Express Initialized');
+
+  await dbLoader();
+  console.log('Database Initialized');
 }
---
+++ src/loaders/mysql.ts
@@ -0,0 +1,18 @@
+import mysql, { Pool } from 'mysql';
+import { DbHandler }   from "../repositories/db.handler";
+
+export default async (): Promise<Pool> => {
+  const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dateStrings: true
+  });
+
+  DbHandler.getInstance(pool);
+
+  return pool;
+}
