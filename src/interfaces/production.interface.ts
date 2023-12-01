b1947b2b0e2a97ad41fbdf78c6df52fb0d56114f
Paul DAO <dao.van.paul@gmail.com>
2023-12-01 17:15:18 -0500

    FEAT collection.repository.ts type with production interface

---
+++ src/interfaces/production.interface.ts
@@ -0,0 +1,11 @@
+export interface Production {
+  id: number
+  creator_id: number
+  royalty_fees: number
+  title: string
+  name_seo: string
+  release_year: number
+  image_id: string
+  created_at: string
+  updated_at: string
+}
--- src/repositories/collection.repository.ts
+++ src/repositories/collection.repository.ts
@@ -1,7 +1,8 @@
 import { AbstractRepository } from "../core/abstract.repository";
 import { TablesEnum }         from "../enums/tables.enum";
+import { Production }         from "../interfaces/production.interface";
 
-export class CollectionRepository extends AbstractRepository<any> {
+export class CollectionRepository extends AbstractRepository<Production> {
   constructor() {
     super(TablesEnum.PRODUCTIONS);
   }
