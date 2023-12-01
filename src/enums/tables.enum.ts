2b0888037a15e6e0e490384fee31a73a0be696aa
Paul DAO <dao.van.paul@gmail.com>
2023-12-01 16:14:02 -0500

    FEAT create enum for table name

---
+++ src/enums/tables.enum.ts
@@ -0,0 +1,11 @@
+export enum TablesEnum {
+  AVAILABLE_SOCIAL_NETWORKS = 'available_social_networks',
+  IMAGES = 'images',
+  NFT_TYPES = 'nft_types',
+  NFTS = 'nfts',
+  OWNERSHIPS = 'ownerships',
+  PRODUCTIONS = 'productions',
+  SOCIAL_NETWORKS = 'social_networks',
+  TRANSACTIONS = 'transactions',
+  USERS = 'users'
+}
