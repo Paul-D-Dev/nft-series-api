fd03e0249504dc23de4042d03c690b15fb1d0bfe
Paul DAO <dao.van.paul@gmail.com>
2023-12-04 15:50:31 -0500

    CHORES abstract model add description fn

--- src/core/abstract.model.ts
+++ src/core/abstract.model.ts
@@ -4,5 +4,11 @@ import { Put, Save } from "../interfaces/http-request";
 export abstract class AbstractModel<T, Db> {
   abstract saveJSONToDb(element: Save<T>): DbSave<Db>;
 
+  /**
+   * When we return the object to store in DB, it does not remove the undefined value
+   * TODO try to find a way to remove undefined values
+   * @param element is a Partial
+   * @return object with undefined values
+   */
   abstract putJSONToDb(element: Put<T>): DbPut<Db>
 }
