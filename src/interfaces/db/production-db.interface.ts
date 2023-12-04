8b61c96d05730c26b588e2d3d8b78c9f09478597
Paul DAO <dao.van.paul@gmail.com>
2023-12-04 14:27:21 -0500

    FEAT interface production db

---
+++ src/interfaces/db/production-db.interface.ts
@@ -0,0 +1,13 @@
+export interface ProductionDb {
+  id: number;
+  creator_id: number;
+  royalty_fees: number;
+  title: string;
+  name_seo: string;
+  release_year: number;
+  image_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+
