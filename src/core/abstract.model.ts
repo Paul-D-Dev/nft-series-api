b9a304483958eb4c249f304614dd5f81705da577
Paul DAO <dao.van.paul@gmail.com>
2023-12-05 14:46:18 -0500

    REFACTOR abstract.model rename fn for readability + protected fn to restrict access

--- src/core/abstract.model.ts
+++ src/core/abstract.model.ts
@@ -3,26 +3,33 @@ import { Put, Save } from "../interfaces/http-request";
 import { removedUndefinedValueInObject } from "../utils/removedUndefinedValueInObject";
 
 export abstract class AbstractModel<T, Db> {
-  abstract saveJSONToDb(element: Save<T>): DbSave<Db>;
+  /**
+   * Map element to save from front end to store in the DB
+   * @param element to save come from Front end
+   * @return element to save in DB with snake_case format
+   */
+  protected abstract mapSaveJSONToDb(element: Save<T>): DbSave<Db>;
 
   /**
-   * When we return the object to store in DB, it does not remove the undefined value
-   * TODO try to find a way to remove undefined values
-   * TODO update with protected abstract
-   * TODO finally just use put
-   * @param element is a Partial
-   * @return object with undefined values
+   * Map element to update from the front end to store in the DB
+   * @param element is a Partial element come from the Front end
+   * @return Object with undefined values
    */
-  abstract putJSONToDb(element: Put<T>): DbPut<Db>
+  protected abstract mapPutJSONToDb(element: Put<T>): DbPut<Db>
 
   protected setUpdateAt(): string {
     return new Date().toISOString().slice(0, 19).replace('T', ' ');
   }
 
   put(element: Put<T>): DbPut<Db> {
-    const mapEl = this.putJSONToDb(element);
+    const mapEl = this.mapPutJSONToDb(element);
+    // @TODO try to fix to remove ts-ignore
     // @ts-ignore
     return removedUndefinedValueInObject(mapEl);
+  }
+
+  save(element: Save<T>): DbSave<Db> {
+    return this.mapSaveJSONToDb(element);
   }
 
 
