93af073f4c4ebb32bfee3aaa97f7cce48c4cb820
Paul DAO <dao.van.paul@gmail.com>
2023-12-05 11:20:01 -0500

    UPDATE abstract model add set update at with iso date + create put fn to return remove undefined value

--- src/core/abstract.model.ts
+++ src/core/abstract.model.ts
@@ -1,5 +1,6 @@
 import { DbPut, DbSave } from "../interfaces/db";
 import { Put, Save } from "../interfaces/http-request";
+import { removedUndefinedValueInObject } from "../utils/removedUndefinedValueInObject";
 
 export abstract class AbstractModel<T, Db> {
   abstract saveJSONToDb(element: Save<T>): DbSave<Db>;
@@ -7,8 +8,22 @@ export abstract class AbstractModel<T, Db> {
   /**
    * When we return the object to store in DB, it does not remove the undefined value
    * TODO try to find a way to remove undefined values
+   * TODO update with protected abstract
+   * TODO finally just use put
    * @param element is a Partial
    * @return object with undefined values
    */
   abstract putJSONToDb(element: Put<T>): DbPut<Db>
+
+  protected setUpdateAt(): string {
+    return new Date().toISOString().slice(0, 19).replace('T', ' ');
+  }
+
+  put(element: Put<T>): DbPut<Db> {
+    const mapEl = this.putJSONToDb(element);
+    // @ts-ignore
+    return removedUndefinedValueInObject(mapEl);
+  }
+
+
 }
