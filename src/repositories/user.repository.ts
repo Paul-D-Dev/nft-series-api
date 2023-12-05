0b06a2c0341ec7e00f3ec532cf8d49be87da5f21
Paul DAO <dao.van.paul@gmail.com>
2023-12-05 11:52:28 -0500

    WIP user repository write get sql request

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -4,6 +4,7 @@ import { UserDb } from "../interfaces/db/user-db.interface";
 import { AbstractModel } from "../core/abstract.model";
 import { UserModel } from "../models/user.model";
 import { TablesEnum } from "../enums/tables.enum";
+import { replaceImageSQL } from "../utils/request-sql/replaceImageSQL";
 
 export class UserRepository extends AbstractRepository<User, UserJSON, UserDb> {
   constructor() {
@@ -11,5 +12,21 @@ export class UserRepository extends AbstractRepository<User, UserJSON, UserDb> {
   }
 
   model: AbstractModel<User, UserDb> = new UserModel();
+
+  GET_ALL = `
+    SELECT t.id,
+           t.name,
+           t.name_seo                   AS nameSeo,
+           t.bio,
+           t.is_verified                AS isVerified,
+           t.contract_address           AS contractAddress,
+           ${replaceImageSQL('t', 'i')} AS image,
+           t.created_at                 AS createdAt,
+           t.updated_at                 AS updatedAt
+    FROM ${this.table} t
+           LEFT JOIN images i ON t.image_id = i.id
+  `;
+
+  GET_BY_ID = `${this.GET_ALL} WHERE t.id = ?;`;
 
 }
