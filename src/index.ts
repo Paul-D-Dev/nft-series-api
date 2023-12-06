f6cdab77e6e19b0c0a7cb3af11e52461031b8b85
Paul DAO <dao.van.paul@gmail.com>
2023-12-06 14:20:59 -0500

    REFACTOR move index.ts start service in src folder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import loaders from "./src/loaders";
-import { ProductionController } from "./src/controllers/production.controller";
-import { UserController } from "./src/controllers/user.controller";
-import { UserSocialNetworkController } from "./src/controllers/user-social-network.controller";
+import loaders from "./loaders";
+import { ProductionController } from "./controllers/production.controller";
+import { UserController } from "./controllers/user.controller";
+import { UserSocialNetworkController } from "./controllers/user-social-network.controller";
 
 dotenv.config();
 
@@ -20,6 +20,5 @@ async function startServer() {
     console.log(`The server is running at http://localhost:${port}`);
   });
 }
-
 
 startServer();
