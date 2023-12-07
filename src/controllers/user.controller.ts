5ab7d49092ff1e3090e6f8cd247a4b10f59229eb
Paul DAO <dao.van.paul@gmail.com>
2023-12-07 16:11:53 -0500

    FEAT user controller post route handle condition body.image

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,5 @@
 import { Application, Request, Response, Router } from "express";
 import { UserService } from "../services/user.service";
-import { commonController } from "../core/common.controller";
 import { CustomError } from "../models/custom-error.model";
 import { ImageService } from "../services/image.service";
 import { Image } from "../interfaces/image.interface";
@@ -10,23 +9,20 @@ export const UserController = (app: Application) => {
   let router: Router = Router();
   const service = new UserService();
   const imageService = new ImageService();
-  commonController(app, service, router);
 
-  router.post('/images', uploadImage(), async (req: Request, res: Response) => {
+  router.post('/', uploadImage(), async (req: Request, res: Response) => {
     try {
       const imageFile = req.file;
       const body = req.body;
-      const imageBody: Partial<Image> = {
-        alt: body?.alt
-      };
-      if (imageFile) {
+      if (body.image && imageFile) {
+        const imageBody: Partial<Image> = {
+          alt: body.image.alt
+        };
         const imageId = await imageService.post(imageBody, imageFile!.path);
-        console.log(imageId);
         const newUser = {
           ...body,
           imageId
         };
-        console.log(newUser);
         await service.post(newUser);
       } else {
         await service.post(body);
