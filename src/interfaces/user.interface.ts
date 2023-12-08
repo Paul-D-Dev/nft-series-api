acee3fe4b7a1b3af71e0f212835e729dfd024462
Paul DAO <dao.van.paul@gmail.com>
2023-12-08 13:22:14 -0500

    REFACTOR user interface create UserToDb to help to put or save user

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,15 +1,18 @@
 import { Image } from "./image.interface";
 import { SocialNetwork } from "./social-network.interface";
 
-export interface User {
+interface _User<ImageType> {
   id: number;
   name: string;
   nameSeo: string | null;
   bio: string | null;
   isVerified: boolean;
   contractAddress: string | null;
-  image: Image | null;
+  image: ImageType | null;
   createdAt: string;
   updatedAt: string | null;
   socialNetworks: SocialNetwork[];
 }
+
+export type User = _User<Image>;
+export type UserToDb = _User<Pick<Image, 'id'>>;
