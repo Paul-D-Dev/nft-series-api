2fe591ce0c48c2d792ecd7af5f4129aecf764b6a
Paul DAO <dao.van.paul@gmail.com>
2023-12-08 13:23:22 -0500

    UPDATE user service add type UserToDb in AbstractService

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,8 @@
 import { AbstractService } from "../core/abstract.service";
-import { User } from "../interfaces/user.interface";
+import { User, UserToDb } from "../interfaces/user.interface";
 import { UserDb } from "../interfaces/db/user-db.interface";
-import { AbstractRepository } from "../core/abstract.repository";
 import { UserRepository } from "../repositories/user.repository";
 
-export class UserService extends AbstractService<User, UserDb> {
-  protected repository: AbstractRepository<User, UserDb> = new UserRepository();
+export class UserService extends AbstractService<User, UserDb, UserToDb> {
+  protected repository = new UserRepository();
 }
