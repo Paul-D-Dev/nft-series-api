a98cff85eba29600e9517899cfefdd2bb9ac51b9
Paul DAO <dao.van.paul@gmail.com>
2023-12-11 14:00:33 -0500

    REFACTOR user controller moves logic post user into user service

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,20 +1,13 @@
 import { Application, Request, Response, Router } from "express";
-import { Save } from "../interfaces/http-request";
-import { Image } from "../interfaces/image.interface";
-import { UserSocialNetwork } from "../interfaces/user-social-network.interface";
-import { User, UserToDb } from "../interfaces/user.interface";
+import { User } from "../interfaces/user.interface";
 import { uploadImage } from "../middlewares/upload-image";
 import { CustomError } from "../models/custom-error.model";
-import { ImageService } from "../services/image.service";
-import { UserSocialNetworkService } from "../services/user-social-network.service";
 import { UserService } from "../services/user.service";
 import { commonDeleteController, commonGetByIdController, commonGetController, commonPutController } from "./common";
 
 export const UserController = (app: Application) => {
   let router: Router = Router();
   const service = new UserService();
-  const userSocialNetworkService = new UserSocialNetworkService();
-  const imageService = new ImageService();
 
   commonGetController(app, service, router);
   commonGetByIdController(app, service, router);
@@ -23,40 +16,10 @@ export const UserController = (app: Application) => {
 
   router.post('/', uploadImage(), async (req: Request, res: Response) => {
     try {
-      let userId: number;
       const userBody: User = req.body;
-
-      // Save Image
       const imageFile: Express.Multer.File | undefined = req.file;
-      if (userBody.image && imageFile) {
-        const imageBody: Partial<Image> = {
-          alt: userBody.image.alt
-        };
-        const imageId: number = await imageService.post(imageBody, imageFile.path);
-        const newUser: UserToDb = {
-          ...userBody,
-          image: {
-            id: imageId
-          }
-        };
-        userId = await service.post(newUser);
-      } else {
-        userId = await service.post(userBody);
-      }
 
-      // Once user id inserted
-      if (userBody.socialNetworks.length > 0 && userId > 0) {
-        // Insert his social networks
-        for (const social of userBody.socialNetworks) {
-          const saveSocial: Save<UserSocialNetwork> = {
-            socialNetworkId: social.socialNetworkId,
-            userId: userId,
-            profileUrl: social.profileUrl,
-            name: social.name
-          };
-          await userSocialNetworkService.post(saveSocial);
-        }
-      }
+      await service.postWithImage(userBody, imageFile);
 
       return res.status(201).end();
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,58 @@
 import { AbstractService } from "../core/abstract.service";
-import { User, UserToDb } from "../interfaces/user.interface";
 import { UserDb } from "../interfaces/db/user-db.interface";
+import { Save } from "../interfaces/http-request";
+import { Image } from "../interfaces/image.interface";
+import { UserSocialNetwork } from "../interfaces/user-social-network.interface";
+import { User, UserToDb } from "../interfaces/user.interface";
 import { UserRepository } from "../repositories/user.repository";
+import { handleCatchError } from "../utils/handleCatchError";
+import { ImageService } from "./image.service";
+import { UserSocialNetworkService } from "./user-social-network.service";
 
 export class UserService extends AbstractService<User, UserDb, UserToDb> {
-  protected repository = new UserRepository();
+  protected readonly repository = new UserRepository();
+  private readonly _imageService = new ImageService();
+  private readonly _userSocialNetworkService = new UserSocialNetworkService();
+
+  async postWithImage(user: User, imageFile?: Express.Multer.File): Promise<number> {
+    try {
+      let userId: number;
+
+      // Save Image
+      if (user.image && imageFile) {
+        const imageBody: Partial<Image> = {
+          alt: user.image.alt
+        };
+        const imageId: number = await this._imageService.post(imageBody, imageFile.path);
+        const newUser: UserToDb = {
+          ...user,
+          image: {
+            id: imageId
+          }
+        };
+        userId = await this.repository.post(newUser);
+      } else {
+        userId = await this.repository.post(user);
+      }
+
+      // Once user id inserted
+      if (user.socialNetworks.length > 0 && userId > 0) {
+        // Insert his social networks
+        for (const social of user.socialNetworks) {
+          const saveSocial: Save<UserSocialNetwork> = {
+            socialNetworkId: social.socialNetworkId,
+            userId: userId,
+            profileUrl: social.profileUrl,
+            name: social.name
+          };
+          await this._userSocialNetworkService.post(saveSocial);
+        }
+      }
+
+      return userId;
+
+    } catch (e) {
+      return handleCatchError(e);
+    }
+  }
 }
