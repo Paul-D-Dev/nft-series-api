aa67f1cfefaf99cb78313fd8e0d90b28eaa2a6b5
Paul DAO <dao.van.paul@gmail.com>
2024-01-09 14:11:04 -0500

    UPDATE pick fn: handle object undefined or null

--- src/utils/pick.ts
+++ src/utils/pick.ts
@@ -1,14 +1,17 @@
 /**
  * Extracts specified keys from an object.
- * @param {Record<string, any>} object - The source object.
+ * @param {Record<string, any> | undefined | null} object - The source object.
  * @param {string[]} keys - An array of keys to extract from the object.
  * @returns {Record<string, any>} - An object containing only the specified keys.
  */
-export const pick = (object: Record<string, any>, keys: string[]) => {
+export const pick = (object: Record<string, any> | undefined | null, keys: string[]) => {
+  if (!object) {
+    return {};
+  }
   // Reduces the keys array to a new object containing only the specified keys.
   return keys.reduce((obj: Record<string, any>, key: string) => {
     // Checks if the key exists in the source object before adding it to the new object.
-    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
       obj[key] = object[key];
     }
     return obj;
